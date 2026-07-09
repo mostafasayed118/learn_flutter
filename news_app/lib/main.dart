09199719e64412c8b0498dd37de75ad7166e48d2
Mustafa Sayed <mustafa_620125@oi.edu.eg>
2026-07-09 15:13:24 +0200

    feat(news_app): add new news application module

---
+++ news_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/screens/homepage.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomePage());
+  }
+}
